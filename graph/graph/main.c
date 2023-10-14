cdf58cf76cf525ab1a4b4ed1e1657ebaa7e048a8
hwKim-dev <k1h6w4@gmail.com>
2023-10-15 00:46:22 +0900

    Create main.c

---
+++ graph/graph/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define N 100
+
+int main()
+{
+    int n = 0;
+    int
+    int graph[N][N];
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; ++i){
+        for(int j = 0; j < n; ++j){
+            graph[i][j] = 0;
+        }
+    }
+
+    for(int i = 0; i < n; ++i){
+        scanf();
+    }
+    return 0;
+}
